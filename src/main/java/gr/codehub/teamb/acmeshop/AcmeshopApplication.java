1d38ec093a54e1a54acda8fea1c94401d742181f
parisk85 <parisk85@gmail.com>
2018-05-11 19:57:14 +0300

    Initial Commit

---
+++ src/main/java/gr/codehub/teamb/acmeshop/AcmeshopApplication.java
@@ -0,0 +1,12 @@
+package gr.codehub.teamb.acmeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeshopApplication.class, args);
+	}
+}
